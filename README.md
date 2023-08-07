eccb7429019764b034f0a287fafe39c2daab3815
praveenreddy-dev <praveen546reddy@gmail.com>
2023-08-07 16:23:24 +0530

    simple-cart-page

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# cart-page
+# Cart Page README
+
+This repository contains the implementation of a Cart Page with basic features like adding items, deleting items, and calculating the total cost. The cart page is a part of a web application and is currently under development.
+
+## Features
+
+- **Add Items:** Users can add items to their shopping cart by clicking the "Add to Cart" button next to each product on the product page. The cart page will display the added items along with their details such as name, price, and quantity.
+
+- **Delete Items:** If users decide to remove an item from their cart, they can click the "Delete" button next to that item on the cart page. The item will be removed from the cart, and the total cost will be updated accordingly.
+
+- **Total Cost:** The cart page will display the total cost of all the items in the cart. This value will be updated in real-time whenever items are added or removed from the cart.
+
+- **Increase Quantity:** This feature is not yet implemented, but it will allow users to increase the quantity of a specific item in the cart. Currently, the quantity can only be set to one when the item is added.
+
+## Development
+
+The development of this Cart Page was inspired by the tutorial from [Web Dev Simplified](https://www.youtube.com/watch?v=YeFzkC2awTM). The tutorial provides valuable insights into building the cart page, and some parts of the code were created following the concepts and guidelines explained in the tutorial.
+
+In this project, I utilized HTML, CSS, and JavaScript to create the cart page. However, I also coded some parts independently to suit the specific needs of the web application I am building.
+
+As mentioned earlier, the feature to increase the quantity of items in the cart is not yet coded. I plan to implement this feature in the near future by adding event listeners and handling user input appropriately.
+
+## How to Use
+
+To use the cart page in your web application, you can follow these steps:
+
+1. Clone this repository to your local machine using `git clone`.
+
+2. Include the necessary HTML, CSS, and JavaScript files in your project. Make sure to link the stylesheets and scripts properly.
+
+3. Integrate the cart page with your product page by adding the "Add to Cart" buttons for each product, and ensure they trigger the appropriate JavaScript functions to add items to the cart.
+
+4. Create a cart container on your cart page where the added items will be displayed along with their details.
+
+5. Implement the "Delete" functionality by adding event listeners to the delete buttons of each item in the cart.
+
+6. Update the total cost dynamically whenever an item is added or deleted from the cart.
+
+7. (Future work) Implement the "Increase Quantity" functionality to allow users to change the quantity of items in the cart.
+
+## Contributions
+
+Contributions to this project are welcome! If you find any bugs, want to add new features, or improve the existing code, feel free to create a pull request. Please make sure to follow the coding conventions and provide clear commit messages for your changes.
+
+## Acknowledgments
+
+I would like to express my gratitude to [Web Dev Simplified](https://www.youtube.com/watch?v=YeFzkC2awTM) for their informative tutorial, which helped me kickstart the development of this cart page.
+
+Happy coding! 🚀
---
+++ script.js
@@ -0,0 +1,109 @@
+let removeListItems = document.getElementsByClassName('btn-danger')
+const cartItems = document.querySelector('.cart-items');
+// console.log(removeListItems)
+for (let i = 0 ; i < removeListItems.length ; i ++){
+    let listItem = removeListItems[i];
+    listItem.addEventListener('click',() =>{
+        
+        let  productContainer = listItem.parentElement.parentElement
+        productContainer.remove();
+        updateCartTotal();
+    })
+}
+
+function updateCartTotal() {
+    let cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    // console.log(cartItemContainer)
+    let cartItemRow =cartItemContainer.getElementsByClassName('cart-row')
+    // console.log(cartItemRow)
+    let total = 0 
+
+    for(let i = 0 ; i < cartItemRow.length ; i++) {
+        let rowItem = cartItemRow[i];
+        let priceElement = rowItem.getElementsByClassName('cart-price')[0];
+        let quantityElement = rowItem.getElementsByClassName('cart-quantity-input')[0];
+    //    console.log(priceElement ,quantityElement)
+        let price = parseFloat(priceElement.innerText.replace('$',''));
+
+        let quantity = quantityElement.value;
+        total = total+ (price* quantity)
+        // Math.round(total)
+
+       
+    }
+    total = total.toFixed(2)
+    document.getElementsByClassName('cart-total-price')[0].innerText = total
+}
+
+// Add event listeners to all "ADD TO CART" buttons
+const addToCartButtons = document.querySelectorAll('.shop-item-button');
+addToCartButtons.forEach(button => {
+    button.addEventListener('click', addToCartClicked);
+});
+
+function addToCartClicked(event) {
+    const button = event.target;
+    const shopItem = button.parentElement.parentElement;
+    // console.log(shopItem)
+    const title = shopItem.querySelector('.shop-item-title').innerText;
+    
+    const price = shopItem.querySelector('.shop-item-price').innerText;
+    const imageSrc = shopItem.querySelector('.shop-item-image').src;
+    addItemToCart(title, price , imageSrc);
+}
+
+function addItemToCart(title, price, imageSrc) {
+    
+    const cartRow = document.createElement('div');
+    cartRow.classList.add('cart-row');
+
+    const cartItems = document.querySelector('.cart-items');
+    const cartItemNames = cartItems.getElementsByClassName('cart-item-title');
+    for (let i = 0; i < cartItemNames.length; i++) {
+        if (cartItemNames[i].innerText === title) {
+            alert('This item is already added to the cart.');
+            return;
+        }
+    }
+
+    const cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>
+    `;
+
+    cartRow.innerHTML = cartRowContents;
+    cartItems.append(cartRow);
+
+    // Attach event listener to the remove button in the newly added row
+    const removeButton = cartRow.querySelector('.btn-danger');
+    removeButton.addEventListener('click', removeCartItem);
+
+    // Recalculate the total price of the cart
+    updateCartTotal();
+}
+function removeCartItem(event) {
+    const buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+}
+function resetState(){
+    while(cartItems.firstChild){  // ☝️  // this loop is for ..as long as there is firstchild element inside answerBtn
+       cartItems.removeChild(cartItems.firstChild);
+      
+    
+    }
+    const totalPrice =document.querySelector('.cart-total');
+    // console.log(totalPrice)
+    total1 = totalPrice.getElementsByClassName('cart-total-price')[0]
+    // console.log(total1)
+    total1.innerText = '$' + 0;
+
+}
+resetState();
